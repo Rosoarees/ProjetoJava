08d8de456ec78b48e49098c658fcf10f22ee561f
Rosoares <rosoares@outlook.com>
2025-08-14 08:54:01 -0300

    Usando o Polimorfismo

---
+++ src/PolimorfismoAnimal/Animal.java
@@ -0,0 +1,7 @@
+public class Animal {
+    public void fazerSom() {
+        System.out.println("O Animal faz um som");
+    }
+    
+   
+    }
---
+++ src/PolimorfismoAnimal/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+        Cachorro cachorro = new Cachorro();
+        Gato gato = new Gato();
+        
+        animal.fazerSom();
+        cachorro.fazerSom();
+        gato.fazerSom();
+        
+       
+        }
+        
+
+    }
